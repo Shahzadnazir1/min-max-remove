ab6fb824b9272323e4c8f247d2bc37db4ab4dd1e
Shahzadnazir1 <shahzadnazir2021@gmail.com>
2025-04-20 21:19:31 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+class program
+{
+    static void Main(string[] args)
+   {
+    int x = +56;
+    int y = -78;
+        //int max = Math.Max(x, y);
+        //int min = Math.Min(x, y);
+        int absolute = Math.Abs(y);
+        //Console.WriteLine("The maximum value is: " + max);
+        // Console.WriteLine("The small value is: " + min );
+         Console.WriteLine("This is sign change value : " +  absolute  );
+
+
+    }
+}
+
+
+
